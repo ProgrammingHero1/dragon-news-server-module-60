2814ef1e9304c6548081452d393bbcbce3cd32a8
Programming Hero <programming.hero1@gmail.com>
2022-10-17 00:20:39 -0500

    basic backend server for dragon news

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json');
+
+app.get('/', (req, res) => {
+    res.send('News API Running');
+});
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log('Dragon News Server running on port', port);
+})
