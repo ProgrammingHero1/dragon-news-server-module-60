bc342f76f91dffe5331bef502e641ca77051786b
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2022-10-18 12:18:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,23 +16,6 @@ app.get('/news-categories', (req, res) => {
     res.send(categories)
 });
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '08') {
-        res.send(news);
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    }
-})
-
-app.get('/news/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedNews = news.find(n => n._id === id);
-    res.send(selectedNews);
-})
-
 app.listen(port, () => {
     console.log('Dragon News Server running on port', port);
 })
